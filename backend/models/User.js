da321bc47a2074787602c3af2cc10bffca88ce16
« marcillyl » <marcillyl@icloud.com>
2022-01-07 05:34:12 -0800

    Create user model

---
+++ backend/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  username: {type: String, required: true, unique: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  isAdmin: {type: Boolean, required: true, default: 0}
+})
+
+module.exports = mongoose.model('User', userSchema)
