08d8bc9788f49ece436e1cc7d52ccc7ae1da07eb
« marcillyl » <marcillyl@icloud.com>
2022-01-07 05:15:51 -0800

    Setup server.js

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const app = require('./app')
+const PORT = process.env.PORT
+app.set(4000)
+const server = http.createServer(app)
+server.listen(4000, () => {
+  console.log(`App listening on port ${PORT || 4000}`)
+})
