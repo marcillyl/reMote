fe5bea5dd70490b8289240cf50cad31aac2ad2f7
« marcillyl » <marcillyl@icloud.com>
2022-01-07 09:30:04 -0800

    Refactor

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,8 @@
-const mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
-  username: {type: String, required: true, unique: true},
-  email: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-  isAdmin: {type: Boolean, required: true, default: 0}
-})
-
-module.exports = mongoose.model('User', userSchema)
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, required: true, default: 0 },
+});
+module.exports = mongoose.model('User', userSchema);
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,6 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/auth.controller')
-
-router.post('/signup', authController.signup)
-router.post('/signin', authController.signin)
-
-module.exports = router
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/auth.controller');
+router.post('/signup', authController.signup);
+router.post('/signin', authController.signin);
+module.exports = router;
