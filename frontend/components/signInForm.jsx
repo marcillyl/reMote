5987ded7be8135895fa3ba6ec9c2b02d8c27874e
« marcillyl » <marcillyl@icloud.com>
2022-01-10 02:41:10 -0800

    Setup Next.js

---
+++ frontend/components/signInForm.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.Form.module.css';
+
+export default function SignInForm() {
+  return (
+    <div>
+      <form className={styles.form}></form>
+    </div>
+  );
+}
---
+++ frontend/components/signUpForm.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.Form.module.css';
+
+export default function SignUpForm() {
+  return (
+    <div>
+      <form className={styles.form}></form>
+    </div>
+  );
+}
