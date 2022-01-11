81dc7c89dfad5c09b8d66517a1bbbf52802f6d48
« marcillyl » <marcillyl@icloud.com>
2022-01-11 07:45:59 -0800

    Refactor user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,9 @@
 const User = require('../models/User');
 exports.deleteUser = async (req, res) => {
-  User.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: '' }))
-    .catch(() => res.status(500));
+  try {
+    await User.deleteOne({ _id: req.params.id });
+    res.status(200).json({ message: '' });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
