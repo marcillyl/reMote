f88d3ffdd5879b3b452fd7466bd08332db10aa1c
« marcillyl » <marcillyl@icloud.com>
2022-01-12 09:57:07 -0800

    Create models

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const projectSchema = mongoose.Schema({
+const taskSchema = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   status: { type: Boolean, required: true, default: 0 },
 });
-module.exports = mongoose.model('Project', projectSchema);
+module.exports = mongoose.model('Task', taskSchema);
